78fcf4e25f908894b3839ee38fe09f1c050841ba
ConanGH-S <blandon0207s@gmail.com>
2023-12-22 12:36:23 -0500

    feat: now mailer send html

--- src/controllers/email.controllers.js
+++ src/controllers/email.controllers.js
@@ -12,15 +12,48 @@ import envConfig from '../config/env.config.js';
 */
 const sendEmail = async (req, res) => {
   const {
-    to, subject, text,
+    to, subject, information,
   } = req.body;
+
+  const HTMLBODY = `<body style="margin: 15rem">
+  <h1 style="text-align: center; font-size: 2rem; margin-bottom: 2rem">Información de nueva solicitud</h1>
+  <p style="font-size: 1.5rem; margin-bottom: 1rem">La persona ${information.name} ha solicitado un nuevo servicio de ${information.services},</p>
+  <table style="border-collapse: collapse; width: 100%; border-radius: 8px; overflow: hidden">
+    <thead>
+      <tr style="background-color: #f0f0f0">
+        <th style="border: 1px solid #ccc; padding: 8px">Nombre/s</th>
+        <th style="border: 1px solid #ccc; padding: 8px">Apellidos</th>
+        <th style="border: 1px solid #ccc; padding: 8px">Tipo de Documento.</th>
+        <th style="border: 1px solid #ccc; padding: 8px">Nº de Documento.</th>
+        <th style="border: 1px solid #ccc; padding: 8px">Nº de Celular</th>
+        <th style="border: 1px solid #ccc; padding: 8px">Teléfono Fijo</th>
+        <th style="border: 1px solid #ccc; padding: 8px">Servicios</th>
+        <th style="border: 1px solid #ccc; padding: 8px">Dirección</th>
+        <th style="border: 1px solid #ccc; padding: 8px">Barrio</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.name}</td>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.lastName}</td>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.document}</td>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.numDocument}</td>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.numPhone}</td>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.numFijo ?? 'N/A'}</td>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.services}</td>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.address}</td>
+        <td style="border: 1px solid #ccc; padding: 8px">${information.neighborhood}</td>
+      </tr>
+    </tbody>
+  </table>
+</body>`;
 
   try {
     await emailConfig.sendMail({
       from: envConfig.mailUser,
       to,
       subject,
-      text,
+      html: HTMLBODY,
     });
     return res.sendStatus(204);
   } catch ({ message }) {
